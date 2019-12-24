2d26e27d2c2c19437510b4cf1df37dc7f2583d6f
yuannian <zyfyuannian@sina.com>
2019-12-24 14:13:33 +0800

    yayaya

---
+++ zhaoyifan/src/com/blb/test/Test.java
@@ -0,0 +1,8 @@
+package com.blb.test;
+
+public class Test {
+    public static void main(String []args){
+        System.out.println("lailailai");
+        System.out.println("youyouyou");
+    }
+}
