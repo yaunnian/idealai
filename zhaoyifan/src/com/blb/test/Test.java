c20ed1c10781f6e832a94fccbc364e2a29440ae8
yuannian <zyfyuannian@sina.com>
2019-12-25 18:46:02 +0800

    12-25代码

--- zhaoyifan/src/com/blb/test/Test.java
+++
@@ -1,8 +0,0 @@
-package com.blb.test;
-
-public class Test {
-    public static void main(String []args){
-        System.out.println("lailailai");
-        System.out.println("youyouyou");
-    }
-}
